05e3b1f3b31379b28d3088807d18e8890fba3e8a
j-ferber <ferberjustin5@gmail.com>
2023-09-18 02:00:22 -0400

    resposive changes project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,23 +11,23 @@ const Projects = () => {
   const {id} = useParams()
   const selectedProject = allProjects.projects.filter(project => id == project.id)[0]
   const listTechnologies = selectedProject.technologies.map((tech, index) => (
-    <li className='ml-10 leading-8 text-lg max-[1024px]:text-base max-[425px]:text-xs' key={index}>{tech}</li>
+    <li className='leading-8 text-lg max-[1024px]:text-base max-[425px]:text-xs' key={index}>{tech}</li>
   ))
 
   return (
     <>
       <ProjectHeader />
       <section className='w-screen min-h-screen flex justify-center items-center py-10'>
-        <div className='w-3/4 min-h-max bg-black bg-opacity-75 p-6 flex flex-col items-center shadow-lg shadow-black rounded-[30px]'>
-          <h2 className='text-[#1d819b] text-3xl font-bold mb-4'>{selectedProject.title}</h2>
+        <div className='w-3/4 min-h-max bg-black bg-opacity-75 p-6 flex flex-col items-center shadow-lg shadow-black rounded-[30px] justify-center'>
+          <h2 className='text-[#1d819b] text-3xl font-bold mb-4 max-[475px]:text-2xl'>{selectedProject.title}</h2>
           <div className="w-full h-full grid cssGrid max-[1024px]:grid-cols-1">
-            <div className='flex justify-center items-center '>
+            <div className='flex justify-center items-center'>
               <img src={selectedProject.source} className='border-4 overflow-hidden h-full object-cover w-full rounded-[30px] border-[#1d819b]' alt="" />
             </div>
-            <div className=' text-white flex flex-col w-[95%] justify-center items-start'>
-              <p className='text-white text-2xl max-[1024px]:mt-6 max-[1024px]:text-lg max-[425px]:text-sm'>{selectedProject.description}</p>
+            <div className='text-white flex flex-col w-[95%] justify-center items-start'>
+              <p className='text-white text-2xl max-[1024px]:mt-6 max-[1024px]:text-lg max-[425px]:text-sm w-full'>{selectedProject.description}</p>
               <h2 className='mt-6 text-xl font-semibold text-[#1d819b] max-[1024px]:text-lg max-[425px]:text-sm'>Technologies:</h2>
-              <ul className='list-disc'>
+              <ul className='list-disc h-full w-full ml-10'>
                 {listTechnologies}
               </ul>
               {selectedProject.note &&
